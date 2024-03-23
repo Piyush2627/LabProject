ffbefe2d93a60b8de268fbcd5de4c4a43f037a90
Piyush2627 <piyushdahiwadkar89@gmail.com>
2024-03-23 17:25:44 +0530

    navigation bar into tailwind csss

---
+++ src/components/common/Navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface NavProps {}
+//import
+
+function Navigation({}: NavProps) {
+  return (
+    <div className=" w-full border-b-2 border-b-black p-2">
+      <div className="m-auto mx-4">
+        <div className=" flex"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,41 +1,43 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-import TopNavbar from '../components/TopNavbar';
-import AddStudentPage from '../components/pages/AddStudentPage/AddStudentPage';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TopNavbar from "../components/TopNavbar";
+import AddStudentPage from "../components/pages/AddStudentPage/AddStudentPage";
 import Dashboard from "../components/pages/Dashboard/Dashboard";
-import MarkAttedence from '../components/pages/MarkAttedence/MarkAttedence';
-import MakeMarksheet from '../components/pages/MakeMarksheet/MakeMarksheet';
+import MarkAttedence from "../components/pages/MarkAttedence/MarkAttedence";
+import MakeMarksheet from "../components/pages/MakeMarksheet/MakeMarksheet";
+import Navigation from "../components/common/Navigation/Navigation";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element:
-            <TopNavbar />,
-        children: [
-            {
-                path: "/",
-                element: <Dashboard />
-            },
-            {
-                path: "/addstudent",
-                element: <AddStudentPage />
-            },
-            {
-                path: "/markattedence",
-                element: <MarkAttedence />
-            },
-            {
-                path: "/makemarksheet",
-                element: <MakeMarksheet />
-            },
-        ]
-    },
+  {
+    path: "/oldservices",
+    element: <TopNavbar />,
+    children: [
+      {
+        path: "/oldservices",
+        element: <Dashboard />,
+      },
+      {
+        path: "/oldservices/addstudent",
+        element: <AddStudentPage />,
+      },
+      {
+        path: "/oldservices/markattedence",
+        element: <MarkAttedence />,
+      },
+      {
+        path: "/oldservices/makemarksheet",
+        element: <MakeMarksheet />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Navigation />,
+  },
 ]);
 
 function Routes() {
-    return (
-        <RouterProvider router={router} />
-    )
+  return <RouterProvider router={router} />;
 }
 
-export default Routes
+export default Routes;
